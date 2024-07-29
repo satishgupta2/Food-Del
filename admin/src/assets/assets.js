509d02fede09a8cd67d50f473cb3eb3770764fd5
satishgupta <satishguptagps@gmail.com>
2024-07-29 16:18:11 +0530

    adminImg-commit

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,17 +1,17 @@
 import logo from './logo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
+import mrbeast from './mrbeast.png'
 
 export const assets ={
     logo,
     add_icon,
     order_icon,
-    profile_image,
     upload_area,
-    parcel_icon
+    parcel_icon,
+    mrbeast
 }
 
 export const url = 'https://food-del-backend-6rv2.onrender.com'
